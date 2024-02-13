4c1ec3e89e6d058bbb86ac232a4c9f72ea4aea0c
SarangaSiriwardhana9 <lasindusaranga99@gmail.com>
2024-02-13 21:51:06 +0530

    create update user profile function

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,15 +3,27 @@ import {useSelector} from 'react-redux';
 import {useRef,useState, useEffect} from 'react';
 import {getDownloadURL, getStorage,ref,uploadBytesResumable} from 'firebase/storage'
 import {app} from '../firebase'
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  signOutUserStart,
+} from '../redux/user/userSlice';
+import {useDispatch} from 'react-redux';
 
 export default function Profile() {
   const fileRef=useRef(null)
-  const {currentUser}=useSelector((state)=> state.user)
+  const {currentUser,loading,error}=useSelector((state)=> state.user)
   const [file, setFile] = useState(undefined);
   const[filePerc,setFilePerc]=useState(0)
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData,setFormData]=useState({})
-  console.log(formData)
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
+
 
   console.log(file);
   console.log(filePerc);
@@ -54,11 +66,46 @@ export default function Profile() {
       }
     );
   };
+  //console log current user
+  console.log(currentUser);
+
+  //handle change
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  //handle submit
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
 
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
 
       <input
           onChange={(e) => setFile(e.target.files[0])}
@@ -92,17 +139,37 @@ export default function Profile() {
       
        </p>
 
-       <input type='text' placeholder='username'
-       className='border p-3 rounded-lg'/>
+       <input type='text' 
+       placeholder='username'
+        id='username'
+       className='border p-3 rounded-lg'
+       defaultValue={currentUser.username}
+       
+        onChange={handleChange}
+       />
 
-        <input type='email' placeholder='email' id='email'
-       className='border p-3 rounded-lg'/>
+        <input type='email'
+         placeholder='email' 
+         id='email'
+       className='border p-3 rounded-lg'
+       defaultValue={currentUser.email}
+       onChange={handleChange}
+       />
 
-      <input type='text' placeholder='password' id='password'
-       className='border p-3 rounded-lg'/>
+<input
+          type='password'
+          placeholder='password'
+          onChange={handleChange}
+          id='password'
+          className='border p-3 rounded-lg'
+        />
 
-       <button className='bg-slate-700 text-white rounded-lg p-3 
-       uppercase hover:opacity-95 disabled:opacity-80'>update</button>
+<button
+          disabled={loading}
+          className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Update'}
+        </button>
 
 
 
@@ -111,6 +178,12 @@ export default function Profile() {
         <span className='text-red-700 cursor-pointer font-semibold'>Delete Account </span>
         <span className='text-red-700 cursor-pointer font-semibold'>Sign Out </span>
       </div>
+      <p className='text-red-700 mt-5'>{error ? error : ''}</p>
+      <p className='text-green-700 mt-5'>
+        {updateSuccess ? 'User is updated successfully!' : ''}
+      </p>
     </div>
   )
 }
+
+/* update */
